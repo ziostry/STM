cd07c1f08fd17861b6de3f87eaa363d71c03b12c
Ziółek <ziolkowskid95@gmail.com>
2020-12-22 16:54:51 +0100

    STM

---
+++ src/main/java/com/example/lab1/StmApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StmApplication.class, args);
+    }
+
+}
